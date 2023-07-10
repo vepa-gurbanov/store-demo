e6874da9cacd54fcae41c6abced3c4b9de72ca36
vepa-gurbanov <tazesalgy@gmail.com>
2023-07-10 10:39:40 +0500

    1.0.1

---
+++ config/language.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'en' => 'English',
+    'ru' => 'Русский язык',
+    'tm' => 'Türkmençe',
+    'tr' => 'Türkçe',
+];
---
+++ config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'ordering' => [
+        'nameAsc' => ['name', 'asc'],
+        'nameDesc' => ['name', 'desc'],
+        'priceLow' => ['price', 'asc'],
+        'priceHigh' => ['price', 'desc'],
+        'newest' => ['id', 'desc'],
+        'oldest' => ['id', 'asc'],
+    ]
+];
